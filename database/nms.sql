2fc45d2c5854fb780329231a4834387aa5d02ca3
Hardik kanajariya <work24to7@gmail.com>
2022-11-19 02:45:58 +0530

    update

--- database/nms.sql
+++ database/nms.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 16, 2022 at 06:33 PM
--- Server version: 8.0.27
--- PHP Version: 8.1.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `nms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
@@ -39,20 +11,6 @@ CREATE TABLE IF NOT EXISTS `admin` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `name`, `phone`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'New Hardik', '9909520639', 'hardik@maill.com', '$2y$10$jLM7x.Ub1f5ahUHg3a4fH.p98tzmesHPwmmBLEBDoZYUQ.okOoTpq', '2022-11-16 04:07:57', '2022-11-16 04:07:57'),
-(2, 'yash', 'Kanajariya', 'yash@mail.com', '$2y$10$6RloSHDQkRk714fxH6JppeeKNvj8dXfzxvfo.8L6N5PCGhG7MGklq', '2022-11-16 10:04:22', '2022-11-16 10:04:22');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -60,22 +18,14 @@ CREATE TABLE IF NOT EXISTS `category` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `category`
---
 
-INSERT INTO `category` (`id`, `name`) VALUES
-(1, 'category 1'),
-(3, '0'),
-(4, '0'),
-(5, 'something'),
-(6, 'hardik');
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `offers`
---
 
 DROP TABLE IF EXISTS `offers`;
 CREATE TABLE IF NOT EXISTS `offers` (
@@ -87,12 +37,6 @@ CREATE TABLE IF NOT EXISTS `offers` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
 
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
@@ -113,11 +57,11 @@ CREATE TABLE IF NOT EXISTS `products` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `review`
---
+DROP TABLE IF EXISTS `purchase`;
+CREATE TABLE IF NOT EXISTS `purchase` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `review`;
 CREATE TABLE IF NOT EXISTS `review` (
@@ -130,20 +74,8 @@ CREATE TABLE IF NOT EXISTS `review` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `review`
---
-
-INSERT INTO `review` (`id`, `uid`, `pid`, `name`, `msg`, `rating`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 'yashKanajariya', 'dummy review updated', 5, '2022-11-16 15:31:49', '2022-11-16 15:31:49');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -170,15 +102,3 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `users_email_unique` (`email`),
   UNIQUE KEY `users_mail_hash_unique` (`mail_hash`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userid`, `first_name`, `last_name`, `address`, `city`, `state`, `zip`, `phone`, `username`, `status`, `email`, `mail_hash`, `email_verified_at`, `password`, `social_id`, `avatar`, `created_at`, `updated_at`) VALUES
-(1, 'updated yash', 'Kanajariya', 'kalyanpur', 'jamanagar', 'gujarat', '361320', '9909520639', 'newyash', 'pending', 'newyash@mail.com', '52f78afeadcc3de04f693224d3dd0116', NULL, '$2y$10$wHlWvtJy.25LAoOboqce0u0OhQItv3d5lh5DD0xQ5FokX0Nghn1Ue', NULL, 'https://api.multiavatar.com/stefan.svg', '2022-11-16 08:49:41', '2022-11-16 08:49:41');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
