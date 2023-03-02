30de6e5938fce1e464d14bdfcad847b19f904e2b
Student <hcollege0@gmail.com>
2023-03-02 12:00:01 +0530

    working on fetch user details by userid

--- api/user/fetch_by_userid.php
+++ api/user/fetch_by_userid.php
@@ -11,29 +11,10 @@ if (empty($fun) || empty($db)) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-    // Authenticating user
-    $fun->verify_token(true);
-    $request = file_get_contents("php://input");
-    $request = json_decode($request);
-
-    // request validator 
-    $validation = $validator->make((array)$request, [
-        'userid' => 'required',
-    ]);
-
-    $validation->validate();
-
-    // handling request errors
-    if ($validation->fails()) {
-        $errors = $validation->errors();
-        echo json_encode($errors->firstOfAll());
-        http_response_code(406);
-        exit;
-    }
+    $userid = $_GET['userid'];
     // fetching review by product id  
-    $data = $db->from('user')->where('userid')->is($request->userid)->select()->all();
+    $data = $db->from('user')->where('userid')->is($userid)->select()->all();
     echo json_encode($data);
 } else {
-
     http_response_code(405);
 }
